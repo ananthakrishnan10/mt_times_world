9775a657c4b35bd5ab09dfd40f4f33d40533769a
Anantha Krishnan <krishnan95.ak@gmail.com>
2025-07-24 09:41:38 +0530

    feat: update config and format pages

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,8 @@
-// eslint.config.js
 import js from '@eslint/js';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
+import pluginImport from 'eslint-plugin-import';
 import prettier from 'eslint-plugin-prettier';
 import { defineConfig } from 'eslint/config';
 
@@ -15,6 +15,8 @@ export default defineConfig([
         ecmaVersion: 'latest',
         sourceType: 'module',
         ecmaFeatures: { jsx: true },
+        project: './tsconfig.json',
+        tsconfigRootDir: process.cwd(),
       },
       globals: globals.browser,
     },
@@ -22,6 +24,7 @@ export default defineConfig([
       js,
       '@typescript-eslint': tseslint.plugin,
       react: pluginReact,
+      import: pluginImport,
       prettier,
     },
     rules: {
@@ -29,7 +32,30 @@ export default defineConfig([
       ...tseslint.configs.recommended.rules,
       ...pluginReact.configs.recommended.rules,
       'prettier/prettier': 'error',
-      'react/react-in-jsx-scope': 'off', // for React 17+
+      'react/react-in-jsx-scope': 'off',
+      'import/no-unresolved': 'error',
+      'import/named': 'error',
+      'import/order': [
+        'warn',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
+          ],
+          'newlines-between': 'always',
+        },
+      ],
+    },
+    settings: {
+      'import/resolver': {
+        typescript: {
+          project: './tsconfig.json',
+        },
+      },
     },
   },
 ]);
